8cd07458eb4ab2d68cbdf758b72fe3d40de42d5e
Shabhaizafar <zafarshabhai03@gmail.com>
2025-07-29 17:01:45 +0530

    inbuilt Function

---
+++ Basic/4.Variables/all.js
@@ -0,0 +1,112 @@
+// variable : 
+
+// var num1 = 12;
+// var ch = 'a';
+// var firstname = "Zafar";
+// var arr = [11,12,13];
+
+// console.log(num1);
+// console.log(ch);
+// console.log(firstname);
+// console.log(arr);
+
+
+
+
+// let num1 = 12;
+// let ch = 'a';
+// let firstname = "Zafar";
+// let arr = [11,12,13];
+
+// console.log(num1);
+// console.log(ch);
+// console.log(firstname);
+// console.log(arr);
+
+
+
+// const num1 = 12;
+// const ch = 'a';
+// const firstname = "Zafar";
+// const arr = [11,12,13];
+
+// console.log(num1);
+// console.log(ch);
+// console.log(firstname);
+// console.log(arr);
+
+
+
+// var : 
+// var n1 = 12;  
+// console.log(n1);
+// // n1 = 100;
+// // console.log(n1);
+// var n1 = 1000;
+// console.log(n1);
+
+
+
+
+// // let n2 = 13;
+// // console.log(n2);
+// // n2 = 200;
+// // console.log(n2);
+// // let n2 = 2000;
+
+
+// const n3 = 14;
+// console.log(n3);
+// n3 = 300;
+// console.log(n3);
+
+
+
+
+
+
+
+// if(condition){
+//     code;
+// }else if(condition2){
+
+// }else{
+//     com
+// }
+
+
+// switch (key) {
+//     case 1:
+        
+//         break;
+//     case "zafar":
+//             break;
+//     case true:
+//             break;
+//     default:
+//         break;
+// }
+
+
+for (let i = 0; i <10; i++) {
+    console.log(i) ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Basic/5.InBuitFuntions/inbuiltFu.js
@@ -0,0 +1,71 @@
+// Function : 
+// It's Perform Some Specific Task. 
+// There are divide in  Two      
+
+
+
+// prompt,alert,confirm
+
+
+// 1. Prompt :->  cancel : null(object)/value(string)
+// in C : scanf()
+
+
+// var v1 = prompt("Enter your Name :");
+
+// console.log(v1);
+
+// var n1 = Number(prompt("Enter The Value of N1 : "));
+// var n2 = Number(prompt("Enter The Value of N2 : "));
+
+// console.log(n1);
+// console.log(n2);
+// console.log(n1+n2);
+
+
+
+// 2. Confirm : true/false
+// console.log(confirm("Are you Ready ? "));
+
+
+
+
+// 3. alert : pop msg 
+// alert("Data added Successfully");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
