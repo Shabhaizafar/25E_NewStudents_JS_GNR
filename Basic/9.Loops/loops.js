751096af62fd0cc385987b3b34ed99a862905302
Shabhaizafar <zafarshabhai03@gmail.com>
2025-08-12 16:49:44 +0530

    Loops

---
+++ Basic/9.Loops/loops.js
@@ -0,0 +1,44 @@
+// for 
+// while 
+// do-while 
+
+
+
+
+// Array  : 
+// for-each
+// for-in 
+// for-of 
+
+// var arr = [11,12,13,14,15];
+// console.log(arr);
+
+// return first : value    second : index    third : self array
+// arr.forEach((element,index,selfarr) => {
+//     console.log(element,index,selfarr);
+// });
+
+
+//  key : similer to index 
+// for (const key in arr) {
+//     console.log(key,typeof key,arr[key]);
+// }
+
+// 
+// for (const element of arr) {
+//     console.log(element);
+// }
+
+
+
+/*
+
+
+1. **Using `forEach`** – You have an array of **restaurant dish prices**. Print each price with its dish number in the format:
+   `Dish 1: ₹250`
+
+2. **Using `for...of`** – You have an array of **daily temperatures**. Calculate and print the **average temperature** for the week.
+
+3. **Using `for...in`** – You have an array of **train departure times**. Print only the times for trains at **even-numbered platforms**.
+
+*/
