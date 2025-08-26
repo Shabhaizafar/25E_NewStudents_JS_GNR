0ababf0412808a614280a2a6d36ca0eaaa95733a
Shabhaizafar <zafarshabhai03@gmail.com>
2025-08-26 16:43:17 +0530

    Generator

---
+++ Basic/10.Functions/GeneratorFu.js
@@ -0,0 +1,115 @@
+// function printAll(){
+//     console.log(1);
+//     console.log(2);
+//     console.log(3);
+//     console.log(4);
+// }
+
+// printAll(); //1
+// printAll(); //2
+
+
+// Generator Fu : 
+
+// 1.
+// function * functionname(){
+    // code
+// }
+
+// 2.
+// function* functionname(){
+    // code
+// }
+
+// 3.
+// function *functionname(){
+    // code
+// }
+
+// 4.
+// function*functionname(){
+    // code
+// }
+
+
+
+// function * GeneratorFu(){
+//     yield 1,
+//     yield 2,
+//     yield "Royal",
+//     yield 4
+// }
+
+
+// var myGen = GeneratorFu();
+
+// console.log(myGen.next().value);
+// console.log(myGen.next().value);
+// console.log(myGen.next().value);
+// console.log(myGen.next().value);
+// console.log(myGen.next().value);
+// console.log(myGen.next().value);
+
+
+
+
+
+
+
+
+
+
+
+
+// function * GeneratorFu(){
+//     yield 1,
+//     yield 2,
+//     yield "Royal",
+//     yield 4
+// }
+
+
+// var myGen = GeneratorFu();
+
+// console.log(myGen.next());  //object   value , done 
+// console.log(myGen.next());  
+// console.log(myGen.next());  
+// console.log(myGen.next());  
+// console.log(myGen.next());  
+
+
+
+// function * GeneratorFu(){
+//     yield 1,
+//     yield 2,
+//     yield "Royal",
+//     yield 4
+// }
+
+
+// var myGen = GeneratorFu();
+
+
+
+
+// function myFu(){
+//     var data = myGen.next();
+//     if (data.done == true){
+//         console.log("Working Stop !!");
+//         return;
+//         }
+//     console.log(data);
+//     myFu();    
+// }
+
+// myFu();
+
+
+
+// Write an arrow function that checks whether a given number is prime.
+
+// Create an arrow function that takes an object and returns all its keys in an array.
+
+
+
+
