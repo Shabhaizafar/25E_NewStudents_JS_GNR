3e30e42749e01dfe0181bc9c1770a19396df0e40
Shabhaizafar <zafarshabhai03@gmail.com>
2025-08-30 14:25:24 +0530

    Callback

---
+++ Basic/10.Functions/callbackFu.js
@@ -0,0 +1,64 @@
+// function addition(n1,n2){
+//     console.log("Addition : ",n1+n2);
+// }
+
+// function main(a,b){
+//     addition(a,b);
+// }
+// main(12,13);
+
+// Callback Functions : 
+
+
+// User Defined : 
+// function addition(n1,n2){ 
+//     console.log("Addition : ",n1+n2);
+// }
+
+
+// function main(callback){    // callback it's a function
+//     callback(1,2);
+// }
+// main(addition);
+
+// var n = prompt();
+// console.log("Value of N : ",n);
+
+
+
+
+
+
+
+
+
+// function main(user_input){
+//     var n = user_input("Enter the Value of N: ");
+//     console.log("Value of N : ",n);
+// }
+// main(prompt);
+
+
+
+
+/*
+
+### Question
+
+You are designing a **movie ticket booking system** in JavaScript.
+
+* Ask the user to **enter the name of the movie** using `prompt()`.
+* Then use `confirm()` to ask if they are **sure about booking** the ticket.
+* If the user confirms, show an `alert()` with a success message.
+* If the user cancels, show an `alert()` with a cancellation message.
+
+👉 **Instruction:**
+
+* Wrap the logic inside a **callback function** that is executed after taking the movie name as input.
+* Demonstrate how the callback function controls what happens after the user provides the input.
+
+
+*/
+
+
+
