0e8e274fb0d3823ac7feeb614f92ce7118373533
Shabhaizafar <zafarshabhai03@gmail.com>
2025-09-01 17:03:37 +0530

    Array Method start

---
+++ Array Methods/p1.js
@@ -0,0 +1,81 @@
+//1. Sort Method :=>
+
+// const arr = [78,10,39,56,45];
+// console.log("Initial Array : ",arr);
+
+// arr.sort();
+
+// console.log("Sorted Array :",arr);
+
+
+
+// const arr = [11,10,111,1001,10001001,10101,10000000000001];
+// console.log("Initial Array : ",arr);
+
+// // asc
+// arr.sort((a,b)=>{
+//     return a-b;
+// });
+
+// desc
+// arr.sort((a,b)=>{
+//     return b-a;
+// });
+
+// console.log("Sorted Array :",arr);
+
+
+// const arr = [11,10,111,1001,10001001,10101,10000000000001];
+// console.log("Initial Array : ",arr);
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//         if(arr[i]>arr[j]){
+//             var temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+//         }
+//     }
+// }
+
+// console.log("Sorted Array :",arr);
+
+
+
+
+// 2. reverse Method :=> 
+// const arr = [78,10,39,56,45];
+// console.log("Array :",arr);
+
+// arr.reverse();
+
+// console.log("Reverse Array : ",arr);
+
+
+
+// 3. every :    boolean : true / false 
+// const arr = [78,10,39,56,45];
+// console.log("Array :",arr);
+
+// var n=25;
+
+// var output = arr.every((value)=>{
+//     return value>n;
+// });
+// console.log(output);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
