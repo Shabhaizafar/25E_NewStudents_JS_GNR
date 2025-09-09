847f27c292572e354a5887bddba0c329563d7879
Shabhaizafar <zafarshabhai03@gmail.com>
2025-09-09 16:10:43 +0530

    Array

---
+++ Array Methods/p3.js
@@ -0,0 +1,63 @@
+var arr = [1,2,3,4,5];
+
+// splice : 
+// console.log(arr);
+
+// arr.splice();
+
+// console.log(arr.slice(2,3));
+// console.log(arr.splice(2,2));
+// console.log(arr);
+
+
+
+
+// delete
+
+// delete arr[0];
+
+// console.log(arr[0]);
+// console.log(arr);
+
+
+// arr[0] = 100;
+// console.log(arr);
+
+
+
+// delete arr;
+console.log(arr);
+
+
+
+
+// var ans = arr.reduce((previousValue,currentValue)=>{
+//     console.log(previousValue,currentValue);
+//     return currentValue+previousValue;
+// });
+
+// var ans = arr.reduce((previousValue,currentValue)=>{
+//     console.log(previousValue,currentValue);
+//     return currentValue+previousValue;
+// },0);
+
+// console.log(ans);
+
+// 11 + 34  = 
+// 34 76
+// 76 2
+// 2 45
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
