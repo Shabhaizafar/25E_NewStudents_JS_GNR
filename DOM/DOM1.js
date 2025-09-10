12d9d9271647fe15343d95ead5f9c7dc2aca0760
Shabhaizafar <zafarshabhai03@gmail.com>
2025-09-10 18:40:29 +0530

    DOM1

---
+++ DOM/DOM1.js
@@ -0,0 +1,39 @@
+// Full HTML File : 
+console.log(document);
+
+
+// How to Access Head Tag : 
+// console.log(document.head);
+
+
+// How to Access Body Tag : 
+// console.log(document["body"]);
+
+
+
+
+// How to access title Tag: 
+// console.log(document.head.title);
+// console.log(document["head"]["title"]);
+
+
+
+//1 using id : 
+// console.log(document.getElementById('heading'));
+
+
+// 2 using class: 
+// console.log(document.getElementsByClassName('class1'));
+// console.log(document.getElementsByClassName('class1')[0]);
+// console.log(document.getElementsByClassName('class1')[1]);
+// console.log(document.getElementsByClassName('class1')[2]);
+
+
+// 3 using tag name : 
+
+// console.log(document.getElementsByTagName('p'));
+// console.log(document.getElementsByTagName('p')[0]);
+console.log(document.getElementsByTagName('p')[1]);
+
+
+
